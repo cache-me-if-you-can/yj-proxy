39e81cf4c55b75e5e4b1551421c5740cf736ed96
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-11-06 15:21:08 -0800

    Implement proxy server code

---
+++ server/app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const httpProxy = require('http-proxy');
+const proxy = httpProxy.createProxyServer();
+const PORT = 3000;
+const ServerOne = 'localhost:3001',
+      ServerTwo = 'localhost:3002',
+      ServerThree = 'localhost:3003',
+      ServerFour = 'localhost:3007';
+
+app.use(express.static(__dirname+'public'));
+
+app.all('/api/symbol/0/day', function(req, res, next){
+    console.log('Redirecting Server1');
+    proxy.web(req, res, {target: ServerOne} );
+
+})
+app.all('/api/volumes/symbols', function(req, res, next){
+    console.log('Redirecting Server2');
+    proxy.web(req, res, {target: ServerTwo} );
+
+})
+app.all('/api/buytest', function(req, res, next){
+    console.log('Redirecting Server2');
+    proxy.web(req, res, {target: ServerThree} );
+
+})
+app.all('/api/alsoBought', function(req, res, next){
+    console.log('Redirecting Server2');
+    proxy.web(req, res, {target: ServerFour} );
+
+})
+
+
+app.listen(PORT, ()=>{
+    console.log('Listening to Port: ', PORT);
+})
