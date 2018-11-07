5958b4abb70c1fec0082d5a9bb07b3297fe11430
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-11-06 16:44:16 -0800

    Refactor with correct server uri

--- server/app.js
+++ server/app.js
@@ -3,19 +3,19 @@ const app = express();
 const httpProxy = require('http-proxy');
 const proxy = httpProxy.createProxyServer();
 const PORT = 3000;
-const ServerOne = 'localhost:3001',
-      ServerTwo = 'localhost:3002',
-      ServerThree = 'localhost:3003',
-      ServerFour = 'localhost:3007';
+const ServerOne = 'http://localhost:3001',
+      ServerTwo = 'http://localhost:3002',
+      ServerThree = 'http://localhost:3003',
+      ServerFour = 'http://localhost:3007';
 
-app.use(express.static(__dirname+'public'));
+app.use(express.static(__dirname+'/../public'));
 
 app.all('/api/symbol/0/day', function(req, res, next){
     console.log('Redirecting Server1');
     proxy.web(req, res, {target: ServerOne} );
 
 })
-app.all('/api/volumes/symbols', function(req, res, next){
+app.all('/api/volumes/symbols/2', function(req, res, next){
     console.log('Redirecting Server2');
     proxy.web(req, res, {target: ServerTwo} );
 
@@ -25,7 +25,7 @@ app.all('/api/buytest', function(req, res, next){
     proxy.web(req, res, {target: ServerThree} );
 
 })
-app.all('/api/alsoBought', function(req, res, next){
+app.all('/api/alsoBought/1', function(req, res, next){
     console.log('Redirecting Server2');
     proxy.web(req, res, {target: ServerFour} );
 
